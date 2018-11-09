d514c7a286a6fa0c562734702984dbc38134d603
EC2 Default User <ec2-user@ip-172-31-46-69.us-east-2.compute.internal>
2018-11-09 22:21:20 +0000

    used express generator to create a new app, will work on loading proper app based on route next

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('This changed!');
-})
+	res.set('Content-Type', 'text/html');
+	res.sendFile('index.html', {root: '/home/ec2-user/web-server/'});
+});
 
 app.listen(3000, () => console.log('Server running on port 3000'));
